4fc238e2ff1fcfb0383b111840c1996d26d58235
gabeFiorot <gjfiorot@lakeheadu.ca>
2021-04-10 16:24:35 -0400

    added components and routing capability

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { LobbiesComponent } from './lobbies/lobbies.component';
+import { BoardComponent } from './board/board.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
 
+
+const routes: Routes = [
+  {path: '', component: LobbiesComponent},
+  {path: 'game-board', component: BoardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+
+  //default redirect
+  {path: '**', redirectTo: ''}
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
