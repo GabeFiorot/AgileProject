f176e832f3591c859d7b2768ac9ab00600696038
gabeFiorot <gjfiorot@lakeheadu.ca>
2021-04-10 16:26:52 -0400

    added components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LobbiesComponent } from './lobbies/lobbies.component';
+import { BoardComponent } from './board/board.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LobbiesComponent,
+    BoardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lobbies/lobbies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lobbies',
+  templateUrl: './lobbies.component.html',
+  styleUrls: ['./lobbies.component.css']
+})
+export class LobbiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
